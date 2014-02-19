e0b5ecc96e078058badad48fb6d6e648a8fbef51
Max Ehrlich <max.ehr@gmail.com>
2014-02-19 15:11:58 -0500

    Added virtual compute function
    Function that can be overriden in child class features that computes the histogram feature on an image. Returns the histogram as a double array

--- Source/Feature/HistogramFeature.hpp
+++ Source/Feature/HistogramFeature.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+namespace cv 
+{
+    class Mat;
+}
+
 namespace ColorTextureShape
 {
     class HistogramFeature
     {
-
+        public: 
+            virtual double *Compute(cv::Mat &img) = 0;
     };
 }
