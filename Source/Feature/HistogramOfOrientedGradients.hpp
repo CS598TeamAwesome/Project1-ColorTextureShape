f086701d9a10c2bc2caeb53ee0deb868d14ed6e6
Max Ehrlich <max.ehr@gmail.com>
2014-02-20 19:30:50 -0500

    Added normalization strategy
    Supports several functions for block normalization with the option to apply a user defined norm function

--- Source/Feature/HistogramOfOrientedGradients.hpp
+++ Source/Feature/HistogramOfOrientedGradients.hpp
@@ -2,11 +2,18 @@
 
 #include "HistogramFeature.hpp"
 #include <opencv2/opencv.hpp>
+#include <functional>
 
 // Defaults taken from optimal findings (Dalal & Triggs CVPR 2005 http://lear.inrialpes.fr/people/triggs/pubs/Dalal-cvpr05.pdf)
 
 namespace ColorTextureShape
 {
+    typedef std::function<void(std::vector<double> &)> NormStrategy;
+
+    void L2Norm(std::vector<double> &);
+    void L1Norm(std::vector<double> &);
+    void L1Sqrt(std::vector<double> &);
+
     class HistogramOfOrientedGradients : public HistogramFeature
     {
         public:
@@ -29,7 +36,12 @@ namespace ColorTextureShape
             cv::Mat My(void) const;
             void My(const cv::Mat &m);
             
-        private:
+            NormStrategy Norm(void) const;
+            void Norm(NormStrategy &n);
+            
+        private:            
+            NormStrategy _Norm = L2Norm;
+            
             cv::Size _CellSize = cv::Size(6, 6);
             cv::Size _BlockSize = cv::Size(3, 3);
             int _Bins = 9;
