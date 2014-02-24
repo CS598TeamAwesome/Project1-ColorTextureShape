24ca6e5adf6db0aa6e716fbe4ec8c7f30d714a8b
anlegong <anlegong@hotmail.com>
2014-02-24 12:22:38 -0500

    Update ChiSquareDistance.cpp

--- Source/Distance/ChiSquareDistance.cpp
+++ Source/Distance/ChiSquareDistance.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 using namespace ColorTextureShape;
 
-// L1 distance  Σ(v1(i)-v2(i))
+//  ∑(i) { 2 * (|HI(i)- HJ(i)|^2])  /  (HI(i) + HJ(i)) }
 double ChiSquareDistance::Compute(const vector<double> &v1, const vector<double> &v2) const {
     if(v1.size() != v2.size()){
         return -1;
     }
 
     double sum = 0;
-    double sub_sum = 0;
+    double sub_sum = 0; // sub_sum use to get the result of 2 * (|HI(i)- HJ(i)|^2])
     
     for(int i = 0; i < v1.size(); i++){
         if(v1.at(i) - v2.at(i) != 0){
             sub_sum = std::abs(v1.at(i) - v2.at(i));
-            sub_sum *= sub_sum;			
+            sub_sum *= sub_sum;		// ^2	
             sum += sub_sum * 2/(v1.at(i) + v2.at(i));
         }
     }
