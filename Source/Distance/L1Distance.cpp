ffca78dc4840af4d99463faa804e3f9efcd88c66
Max Ehrlich <max.ehr@gmail.com>
2014-02-24 09:16:45 -0500

    Added L1Distance

---
+++ Source/Distance/L1Distance.cpp
@@ -0,0 +1,21 @@
+#include "L1Distance.hpp"
+#include <cmath>
+
+using namespace std;
+using namespace ColorTextureShape;
+
+// L1 distance  Σ(v1(i)-v2(i))
+double L1Distance::Compute(const vector<double> &v1, const vector<double> &v2) const {
+    if(v1.size() != v2.size()){
+        return -1;
+    }
+
+    double sum = 0;
+
+    for(int i = 0; i < v1.size(); i++){
+        if(v1.at(i) - v2.at(i) != 0){
+            sum += std::abs(v1.at(i) - v2.at(i));
+        }
+    }
+    return sum;
+}
