c9afaf667ce8b33d84e9569bb14851a56f3321bb
anlegong <anlegong@hotmail.com>
2014-02-24 12:20:51 -0500

    Update L2Distance.cpp

--- Source/Distance/L2Distance.cpp
+++ Source/Distance/L2Distance.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace ColorTextureShape;
 
-// L1 distance  Σ(v1(i)-v2(i))
+// L1 distance  sqrt{ ∑(i) |HI(i) - HJ(i)|^2  }
 double L2Distance::Compute(const vector<double> &v1, const vector<double> &v2) const {
     if(v1.size() != v2.size()){
         return -1;
@@ -13,7 +13,7 @@ double L2Distance::Compute(const vector<double> &v1, const vector<double> &v2) c
     double sum = 0;
 
     for(int i = 0; i < v1.size(); i++){
-        if(v1.at(i) - v2.at(i) != 0){
+        if(v1.at(i) - v2.at(i) != 0){ //HI(i) - HJ(i) = 0 , no need to add to sum.
             sum += std::abs(v1.at(i) - v2.at(i));
             sum *= sum;
         }
