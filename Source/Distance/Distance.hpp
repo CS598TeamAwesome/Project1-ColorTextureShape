faa22e446bfa600f1f5916aa31dfb53f298387fa
Max Ehrlich <max.ehr@gmail.com>
2014-02-24 09:16:37 -0500

    Filled in defintion of abstract Distance class

--- Source/Distance/Distance.hpp
+++ Source/Distance/Distance.hpp
@@ -1,11 +1,14 @@
 #pragma once
+#include <vector>
 
 namespace ColorTextureShape
 {
 
 	class Distance
 	{
-		
+        public:
+            double Compute(const std::vector<double> &v1, const std::vector<double> &v2) const;
+            
 	};
 
 }
